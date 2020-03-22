234437a942d3c0e1415ed1416718f50394bf85a4
Miguel Alfredo <imiked.mail@gmail.com>
2020-03-21 19:34:39 -0600

    Add devise users (#3)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    registration_attributes = [:first_name, :last_name]
+    devise_parameter_sanitizer.permit(:sign_up, keys: registration_attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: registration_attributes)
+  end
 end
